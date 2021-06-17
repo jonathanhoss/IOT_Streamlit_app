01d0d82a279026c4174bf104f8349cea347232f8
Jonathan <jonah.hoss@gmail.com>
2021-06-17 14:48:38 +0200

    Karte um Hum & Druck erweitert

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ def main():
     ## Interaktive Karte
     if st.button('Zeige Geo-Karte'):
         st.write("""
-        # Letzen Daten von Messtationen
+        ## Letzen Daten von Messtationen
         """)
         p = bokeh_map_test.create_map()
         st.bokeh_chart(p, use_container_width=True)
@@ -27,12 +27,16 @@ def main():
     name = st.selectbox('Select name', name_list, index=8)
 
     st.write(f"""
-    # Datensätze von {name}
+    ## Datensätze von {name}
     """)
     df = pd.DataFrame(database_conn.get_values(name), columns=['Time','Ort','Temperatur','Feuchte','Druck'])
-    st.dataframe(df)
-    st.markdown(my_code_snippets.get_table_download_link(df), unsafe_allow_html=True)
-
+    st.dataframe(df.sort_index(ascending=False))
+    # DOWNLOAD
+    st.markdown(my_code_snippets.get_table_download_link(df, name), unsafe_allow_html=True)
+    
+    st.write(f"""
+    ## Chronologisches Diagramm der Wetterdaten von {name}
+    """)
     new_df = df[df.Time > pd.to_datetime("23/5/2021")] #Dataframe nur mit Daten seit diesem Jahr 
 
     fig = make_subplots(rows=2, cols=1)
@@ -41,13 +45,19 @@ def main():
     fig.add_trace(go.Scatter(x=new_df.Time, y=new_df.Druck, name='Druck'), row=2, col=1)
     st.plotly_chart(fig, use_container_width=True)
 
-    import matplotlib.pyplot as plt
-    plt.plot(df.index, df.Temperatur, label='Temperatur')
-    plt.plot(df.index, df.Feuchte, label='Feuchte')
-    #plt.plot(new_df.index, new_df.Druck, label='Druck')
-    plt.legend(loc='upper left')
-    plt.title(f"Termeraturverlauf von {name}")
-    st.pyplot(plt)
+    st.write("""
+    ## Graph mit ID als X-Achse
+    Falls die Zeitdaten nicht richtig erfasst wurden kann es vorkommen, dass der obere Graph nicht angezeigt wird. Dann kann hier der Graph mit der ID als X-Achse ausgegeben werden.
+    """)
+    if st.button('Hier anzeigen'):
+        import matplotlib.pyplot as plt
+        plt.plot(df.index, df.Temperatur, label='Temperatur [°C]')
+        plt.plot(df.index, df.Feuchte, label='Feuchte [%]')
+        #plt.plot(new_df.index, new_df.Druck, label='Druck')
+        plt.legend(loc='upper left')
+        plt.title(f"Termeraturverlauf von {name}")
+        plt.xlabel("ID")
+        st.pyplot(plt)
 
 if __name__ == "__main__":
     main()
--- my_code_snippets.py
+++ my_code_snippets.py
@@ -1,5 +1,5 @@
 import base64
-def get_table_download_link(df):
+def get_table_download_link(df, name):
     #https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806/2
     """Generates a link allowing the data in a given panda dataframe to be downloaded
     in:  dataframe
@@ -7,5 +7,5 @@ def get_table_download_link(df):
     """
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-    href = f'<a href="data:file/csv;base64,{b64}" download="wi4future.csv">CSV Datei Herunterladen</a>'
+    href = f'<a href="data:file/csv;base64,{b64}" download="{name}_wi4future.csv">CSV-Datei Herunterladen</a>'
     return href
